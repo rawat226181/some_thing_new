ee2b1dd44eb34800ea106bd9af8a1caf59ad184d
Deepak Rawat <161299939+rawat226181@users.noreply.github.com>
2025-05-24 22:45:02 +0530

    we just setup the react app

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom';
+function Navbar(){
+    return(
+    <nav style={{padding : '10px' , background: '#ddd' }}>
+        <Link to = "/home">Home</Link>
+        <Link to = "/about">About </Link>
+        <Link to = "/project">Project</Link>
+        <Link to = "/Contact">Contact</Link>
+
+    </nav>
+    )
+}
+
+export default Navbar 
